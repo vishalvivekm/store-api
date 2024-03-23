ea18d6c75c63f32d276c6b4e92e047867297c7ed
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-03-23 18:49:10 +0530

    add pagination
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,11 @@ const Product = require('../models/product')
 
  const getAllProductsStatic = async (req, res) => {
 
-    const products = await Product.find({}).select('name price') // ('-name -price') excludes name and price properties from response :)
+    const products = await Product.find({})
+        .sort('name')
+        .select('name price') // ('-name -price') excludes name and price properties from response :)
+        .limit(10)
+        .skip(5)
     res.status(200).json({products, nbHits: products.length})
 
 }
@@ -35,6 +39,14 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList)
     }
+    const page = Number(req.query.page) || 1 // req.query values: string type
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+// 23
+// 4 7 7 7 2
+
     const products = await result
     res.status(200).json({products, nbHits: products.length})
 
@@ -45,3 +57,4 @@ module.exports = {
 }
 
 // ?fields=company,rating
+// {{URL}}/products?sort=-rating&featured=false&fields=name&limit=4&page=3
