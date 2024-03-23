583623e02d6176513b244142010d7b455189ae29
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-03-23 15:59:04 +0530

    controller: supporting only featured query param
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,66 @@
+const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'products testing route'})
+   const products = await Product.find({name: "wooden desk"}) // just fin({}) returns everything
+    res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    const {featured} = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true:false;
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
     getAllProductsStatic, getAllProducts
 }
+
+
+
+
+
+
+
+
+
+
+
+/*const getAllProducts = async (req, res) => {
+    const products = await Product.find(req.query)
+    res.status(200).json({products, nbHits: products.length})
+}*/
+
+
+// products?featured=true&page=2 // req.query has value: {featured: 'true', page: '2'} // but we don't have any property by 'page' name, so moongoose v5 returns an empty array
+//const products = await Product.find(req.query)
+// res.status(200).json({products, nbHits: products.length})
+// returns:
+/*
+*{
+    "products": [],
+    "nbHits": 0
+}
+* however, mongoose v6 just ignores it
+* */
+
+
+
+//
+//
+// const getAllProductsStatic = async (req, res) => {
+//     throw new Error("testing async errros")
+//     res.status(200).json({msg: 'products testing route'})
+// }
+// const getAllProducts = async (req, res) => {
+//     res.status(200).json({msg: 'products route'})
+// }
+//
+// module.exports = {
+//     getAllProductsStatic, getAllProducts
+// }
